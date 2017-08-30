2a5d7cb4fc5d66eb0b9cc812cfff57a56ecf5b89
asadisaghar <asadisaghar@gmail.com>
2017-08-30 22:28:20 +0200

    rmse calaulation based on inversed yhat

--- play/lstm.py
+++ play/lstm.py
@@ -96,16 +96,16 @@ yhat = model.predict(test_X)
 test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 # invert scaling for forecast
 inv_yhat = np.concatenate((yhat, test_X[:, 1:]), axis=1)
-# inv_yhat = test_scaler.inverse_transform(inv_yhat)
-# inv_yhat = inv_yhat[:,0]
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
 # invert scaling for actual
 test_y = test_y.reshape((len(test_y), 1))
 inv_y = np.concatenate((test_y, test_X[:, 1:]), axis=1)
-# inv_y = test_scaler.inverse_transform(inv_y)
-# inv_y = inv_y[:,0]
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
 # calculate RMSE
-#rmse = sqrt(sklearn.metrics.mean_squared_error(inv_y, inv_yhat))
-rmse = sqrt(sklearn.metrics.mean_squared_error(test_y, yhat))
+rmse = sqrt(sklearn.metrics.mean_squared_error(inv_y, inv_yhat))
+#rmse = sqrt(sklearn.metrics.mean_squared_error(test_y, yhat))
 print('Test RMSE: %.3f' % rmse)
 
 plt.title('Test RMSE: %.3f' % rmse)
